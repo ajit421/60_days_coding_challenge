c07f5a4a5caab315587d92c15136f56b23cf791c
mrDark <110296927+ajit421@users.noreply.github.com>
2023-12-12 20:29:50 +0530

    Add files via upload

---
+++ nestedIfElse.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter your age: ";
+    cin>>age;
+
+    if(age>=18){
+
+        if(age>=18 && age<=60){
+        cout<<"Eligible for organizational work";
+    }
+
+         else{
+        cout<<"Age is not satisfactory as per the organizational work";
+    }
+    }
+
+else{
+    cout<<"Consisdered as miner u can't force then the work ";
+}
+} 
