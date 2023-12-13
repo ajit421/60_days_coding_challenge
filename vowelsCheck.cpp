676f562863a96dd33ba8528897ef219d2e0d8b4a
mrDark <110296927+ajit421@users.noreply.github.com>
2023-12-13 12:42:14 +0530

    Add files via upload

---
+++ vowelsCheck.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+
+    cout << "Enter an alphabet: " << endl;
+    cin >> op;
+
+    switch (op) {
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+            cout << "Your character is a vowel";
+            break;
+
+        default:
+            cout << "Your character is a consonant";
+            break;
+    }
+
+    return 0;
+}
