edea6f81544c128a9de4b9d884a31bf0bdb07441
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2023-12-26 16:13:34 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n=3,m=6;
+    
+    for(int i=1;i<=3;i++){
+        for(int j=1;j<=6;j++){
+        cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
