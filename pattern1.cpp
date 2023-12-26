00a85b60de64dcc0983be60ce66e257f35eb7f0c
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2023-12-26 16:18:57 +0530

    Add files via upload

--- pattern1.cpp
+++ pattern1.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 int main(){
 
-    int n=3,m=6;
-    
-    for(int i=1;i<=3;i++){
-        for(int j=1;j<=6;j++){
+    int n,m;
+    cout<<"Enter row: ";
+    cin>>n;
+
+    cout<<"Enter colum: ";
+    cin>>m;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
         cout<<"*";
         }
         cout<<endl;
