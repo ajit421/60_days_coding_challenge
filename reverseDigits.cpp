6b8cdbb36408a0bcd6f2c92a339b0e0411f775d3
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2023-12-28 22:20:39 +0530

    Add files via upload

---
+++ reverseDigits.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,reverse=0,lastdigit;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while(n>0){
+        lastdigit=n%10;
+        reverse=reverse*10+lastdigit;
+        n/=10;
+    }
+
+    cout << "The reverse of the number is " << reverse << endl;
+
+    return 0;
+}
