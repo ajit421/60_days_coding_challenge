6acd81488b3f1faef4b7cd3e645351f82c27f355
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2024-01-05 22:47:39 +0530

    Add files via upload

---
+++ ASCII.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+   
+    char myChar = 'A'; 
+
+    for (int i=0; i <26; i++) {
+        
+        cout <<(char)('A'+i) << "\t";
+    }
+
+    return 0;
+}
