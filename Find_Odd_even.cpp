3d25881bfe46f79e6858a433ed87c6ecaa8a870c
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2024-01-07 23:15:58 +0530

    Add files via upload

---
+++ Find_Odd_even.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number,ans;
+
+    cout<<"Enter your Integer: ";
+    cin>>number;
+
+    if(number%2==0){
+        cout<<"Even";
+
+    }
+    else{
+        cout<<"Odd";
+    }
+    
+}
