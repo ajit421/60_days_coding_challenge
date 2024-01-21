b1e6819f373f2711cf5dd6b952aee9acda282eee
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2024-01-21 18:57:53 +0530

    Add files via upload

---
+++ Q2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int l,b,area,par;
+    cout<<"\nEnter the value of l & b";
+    cin>>l>>b;
+
+    area=l*b;
+    par=2*(l+b);
+
+    cout<<"\n area :"<<area;
+    cout<<"\n parimeter :"<<par;
+
+    // // float cirf=2*3.1426*r;
+    // float area=3.1426*r*r;
+
+    // cout<<"\ncircumference :"<<cirf;
+    // cout<<"\narea :"<<area;
+}
