b6aa71cba8c481764b37ab43c999e51c812e9f00
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2025-04-07 22:58:35 +0530

    referance

---
+++ reference.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=10;
+    int y=x;
+    cout<<x<<endl;
+    cout<<y<<endl;
+    x=20;
+    cout<<x<<endl;
+    cout<<y<<endl;
+    return 0;
+
+}
